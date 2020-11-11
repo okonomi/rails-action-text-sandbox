e5a428c010941c1be427b5fbc7bb028eddc9338a
okonomi <okonomi@oknm.jp>
2020-11-11 21:45:00 +0900

    refactor: split file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,31 +19,4 @@ require("channels")
 require("trix")
 require("@rails/actiontext")
 
-const getCurrentLine = (editor) => {
-  const document = editor.getDocument()
-  const range = editor.getSelectedRange()
-  const ret = document.expandRangeToLineBreaksAndSplitBlocks(range)
-  return document.getStringAtRange(ret.range)
-}
-
-document.addEventListener("trix-selection-change", (event) => {
-  const editor = event.target.editor
-  console.log(editor.getSelectedRange())
-  console.log(getCurrentLine(editor))
-})
-
-document.addEventListener("trix-change", (event) => {
-  const editor = event.target.editor
-  const currentLine = getCurrentLine(editor)
-  if (currentLine.match(/^# /)) {
-    console.log("heading1")
-    editor.deleteInDirection("backward")
-    editor.deleteInDirection("backward")
-    editor.activateAttribute("heading1")
-  } else if (currentLine.match(/^- /)) {
-    console.log("bullet list")
-    editor.deleteInDirection("backward")
-    editor.deleteInDirection("backward")
-    editor.activateAttribute("bullet")
-  }
-})
+import "../src/trix-markdown-shortcut"
